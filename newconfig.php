d74ade080ff18a43d62efbf27f98ef5988376821
AdamAdham <92235117+AdamAdham@users.noreply.github.com>
2022-09-09 22:21:15 +0200

    Delete newconfig.php

--- newconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $user = 'root';
-    $pass = '';
-    $db = 'users';
-    $db = new mysqli('127.0.0.1:3306', $user, $pass, $db) or die("Error");
-
-    echo("Connected Successfully<br>")
-?>
