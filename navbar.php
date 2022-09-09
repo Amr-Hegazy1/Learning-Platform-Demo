5028aa3954fe7bdbf4a6978b4a8329a342582178
AdamAdham <92235117+AdamAdham@users.noreply.github.com>
2022-09-09 22:20:59 +0200

    Delete navbar.php

--- navbar.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['loggedin'])){$u = $_SESSION['loggedin'];}
-if(isset($_SESSION['assistantloggedin'])){$as = $_SESSION['assistantloggedin'];}
-if(isset($_SESSION['adminloggedin'])){$ad = $_SESSION['adminloggedin'];}
-if($u){}elseif($as){}elseif($ad){}
- ?>
